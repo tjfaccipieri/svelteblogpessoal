1f94f9de3c04ab45111ad38767abd815227fe408
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-08-15 11:38:36 -0300

    teste de request com github api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,19 @@
-Aqui vai vir o conteudo principal
+<script>
+  import axios from 'axios'
+	import { onMount } from 'svelte';
+
+  let data = {}
+
+  onMount(async () => {
+    try {
+      let response = await axios.get('https://api.github.com/users/tjfaccipieri')
+      data = response.data
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  })
+
+</script>
+
+{JSON.stringify(data)}
