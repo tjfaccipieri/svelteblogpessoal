469ded6e99f917d28503d6645562d49eab078d74
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-08-15 08:29:58 -0300

    Estrutura básica de navbar + footer

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,5 @@
+<div class="flex bg-purple-950 text-gray-50 font-semibold py-4 text-lg">
+  <div class="container mx-auto">
+    thiago
+  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
-navbar
+<script>
+	import Footer from '$lib/Footer.svelte';
+import Navbar from '$lib/Navbar.svelte';
+  import '../app.css'
+</script>
+
+<Navbar />
 
 <slot />
 
-footer
+<Footer />
