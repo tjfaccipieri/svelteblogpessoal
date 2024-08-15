fbb02a58c207a453a5ad93e03824eee9da748f77
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-08-15 10:09:15 -0300

    menu dropdown da navbar para o perfil de usuario

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,9 +1,13 @@
+<script>
+	import Profile from "./Profile.svelte";
+
+</script>
 <div class="flex bg-slate-800 text-gray-50 font-semibold py-2 text-lg">
   <div class="container mx-auto flex items-center justify-between">
     <span class="font-bold text-3xl">{'<TJ />'}</span>
     <ul class="text-lg flex gap-6">
       <li>Home</li>
-      <li>Perfil</li>
+      <Profile />
     </ul>
   </div>
 </div>
