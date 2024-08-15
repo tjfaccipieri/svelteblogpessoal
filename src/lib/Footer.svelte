3e00d085463b79ed2c08374869f406a2850c8a4a
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-08-15 09:53:55 -0300

    ajustado cores do footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -5,7 +5,7 @@
   import { IoLogoGithub } from "svelte-icons-pack/io";
 </script>
 
-<footer class="bg-purple-950 text-gray-50 font-semibold py-2 text-lg">
+<footer class="bg-slate-800 text-gray-50 font-semibold py-2 text-lg">
   <div class="container mx-auto">
     <h4 class="flex items-center gap-2 justify-center">Todos os direitos reservados. Thiago Faccipieri @ {ano}  
       <div class="hover:text-gray-500 cursor-pointer transition-colors">
