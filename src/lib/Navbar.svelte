6fc79e19e12d8415256b56072e9ddbf8aad79d45
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-08-15 12:14:13 -0300

    ajustado links da navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -5,9 +5,9 @@
 <div class="flex bg-slate-800 text-gray-50 font-semibold py-2 text-lg">
   <div class="container mx-auto flex items-center justify-between">
     <span class="font-bold text-3xl">{'<TJ />'}</span>
-    <ul class="text-lg flex gap-6">
-      <li>Home</li>
+    <div class="text-lg flex gap-6">
+      <a href='/'>Home</a>
       <Profile />
-    </ul>
+    </div>
   </div>
 </div>
