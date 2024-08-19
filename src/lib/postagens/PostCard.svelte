d45d811941be4a1706837ada6bad81b3dacda8a9
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2024-08-19 10:14:15 -0300

    ajustado layout do card de Postagem

--- src/lib/postagens/PostCard.svelte
+++ src/lib/postagens/PostCard.svelte
@@ -6,12 +6,17 @@
 
 <div class="flex flex-col rounded border border-slate-800 p-2 w-1/3 mx-auto bg-white">
   <div class="flex gap-3 items-center border-b-2 pb-2">
-    <img src={post.usuario.foto} alt="" class="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9">
-    <p class="capitalize font-semibold">{post.usuario.nome}</p>
+    <img src={post.usuario?.foto} alt="" class="flex-shrink-0 object-cover mx-1 rounded-full w-9 h-9">
+    <p class="capitalize font-semibold">{post.usuario?.nome}</p>
+    <span class="ml-auto">{new Intl.DateTimeFormat('pt-BR', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+    timeZone: 'America/Sao_Paulo',
+  }).format(new Date(post.data))}</span>
   </div>
   <div>
     <p>{post.titulo}</p>
-    <p>{post.texto}</p>
+    <pre class="text-wrap font-sans">{post.texto}</pre>
     <p>Tema: {post.tema.descricao}</p>
   </div>
 </div>
